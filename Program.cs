5a1648d722e86ec1b79bf7fe3a8014e944fe10e0
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2025-01-29 16:07:20 +0700

    [INIT]

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CSharpImplementation;
+using Data;
+using System.Data.Common;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DBConnection dBConnection = DBConnection.Instance();
+
+        dBConnection.Server = "localhost";
+        dBConnection.DatabaseName = "modul15";
+        dBConnection.UserName = "root";
+        dBConnection.Password = "satria2133";
+
+
+        if (dBConnection.IsConnect())
+        {
+            Console.WriteLine("Connection established successfully!");
+
+
+            dBConnection.Close();
+            Console.WriteLine("Connection closed.");
+        }
+        else
+        {
+            Console.WriteLine("Failed to establish a connection.");
+        }
+    }
+}
